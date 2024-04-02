706bb6a085fff6b90beb36ba144ac07d78a8a6a9
Hans Arthur Cupiterson <125373236+KetsunoAna117@users.noreply.github.com>
2024-04-02 08:28:49 +0700

    Initial Commit

---
+++ MySIG/MySIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySIGApp.swift
+//  MySIG
+//
+//  Created by Hans Arthur Cupiterson on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MySIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
